2b5305f4e2bc4d9aec2136bee2bf97fc1f6e894d
shivrjpatil <shivrajp972@gmail.com>
2023-02-09 14:53:44 +0530

    Added Program to show top 2 data Who is less 60 age

---
+++ PersonDataManagement/PersonDataManagement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonDataManagement
+{
+    internal class PersonDataManagement
+    {
+        public static void RetrieveTop2Below60(List<Person> list)
+        {
+            var top2 = list.Where(p => p.Age < 60).OrderBy(p => p.Age).Take(2);
+            Console.WriteLine("\nTop 2 Person less than Age 60");
+            Program.DisplayPersonDetails(top2);
+        }
+    }
+}
--- PersonDataManagement/Program.cs
+++ PersonDataManagement/Program.cs
@@ -14,6 +14,7 @@ namespace PersonDataManagement
             List<Person> listOfPerson = new List<Person>();
             //UC1 - Creating Person Class and Adding Data
             AddingPersonDetails(listOfPerson);
+            PersonDataManagement.RetrieveTop2Below60(listOfPerson);
             Console.ReadLine();
         }
         public static void AddingPersonDetails(List<Person> list)
